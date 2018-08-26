45106e66fb881990a1c7319119f854e9f252da15
zhangtianqiu <840666866@qq.com>
2018-08-26 14:09:42 +0800

    code review

--- app/src/main/java/com/code/mvvm/core/data/pojo/material/MaterialTypeVo.java
+++ app/src/main/java/com/code/mvvm/core/data/pojo/material/MaterialTypeVo.java
@@ -1,11 +1,10 @@
 package com.code.mvvm.core.data.pojo.material;
 
-import com.code.mvvm.core.data.pojo.BaseVo;
 
 import java.io.Serializable;
 import java.util.List;
 
-public class MaterialTypeVo extends BaseVo implements Serializable
+public class MaterialTypeVo implements Serializable
 {
 
     public DataEntity data;
